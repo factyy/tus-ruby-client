ba878976337ffda9a4071581d82f56ead773aa32
factyy <factyy@gmail.com>
2020-06-15 15:29:40 +0300

    Add the file creation code (via tus extension)

--- lib/tus/client.rb
+++ lib/tus/client.rb
@@ -1,22 +1,33 @@
 # frozen_string_literal: true
 
-require 'tus/client/version'
-
 require 'net/http'
+require 'base64'
+
+require 'tus/client/version'
 
 module Tus
   class Client
+    # 100 MiB is ok for now...
+    CHUNK_SIZE = 100 * 1024 * 1024
+    TUS_VERSION = '1.0.0'
+
     def initialize(server_url)
       @server_uri = URI.parse(server_url)
 
-      @http = Net::HTTP.new(@server_uri.host, @server_uri.port)
-      @capabilities = capabilities # we cache this value for further use
+      # better to open the connection now
+      @http = Net::HTTP.start(@server_uri.host, @server_uri.port)
+      # we cache this value for further use
+      @capabilities = capabilities
     end
 
     def upload(file_path)
-      raise 'New file uploading not supported!' unless file_exists?(file_path) || @capabilities.include?('creation')
+      raise 'No such file!' unless File.file?(file_path)
 
-      create_remote(file_path) unless file_exists?(file_path)
+      io = File.open(file_path, 'rb')
+
+      uri = create_remote(File.basename(file_path), File.size(file_path))
+
+      io.close
     end
 
     private
@@ -29,12 +40,24 @@ module Tus
       response['Tus-Extension']&.split(',')
     end
 
-    def file_exists?(file_path); end
+    def create_remote(file_name, file_size)
+      unless @capabilities.include?('creation')
+        raise 'New file uploading not supported!'
+      end
 
-    def create_remote(file_path); end
+      request = Net::HTTP::Post.new(@server_uri.request_uri)
+      request['Content-Length'] = 0
+      request['Upload-Length'] = file_size
+      request['Tus-Resumable'] = TUS_VERSION
+      request['Upload-Metadata'] = "filename: #{Base64.strict_encode64(file_name)},is_confidential"
 
-    def file_offset(file_path); end
+      response = @http.request(request)
 
-    def resume_upload(file_path); end
+      p response
+    end
+
+    def file_offset(file_uri); end
+
+    def upload_chunk(file_uri, io); end
   end
 end
